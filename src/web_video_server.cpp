12820232518f3495b7c9838adca22d0b8992203c
Jihoon Lee <jihoonlee.in@gmail.com>
2017-11-20 16:36:34 +0000

    Rebase from 'debian/kinetic/web_video_server'

--- src/web_video_server.cpp
+++ src/web_video_server.cpp
@@ -28,11 +28,14 @@ static bool ros_connection_logger(async_web_server_cpp::HttpServerRequestHandler
   try
   {
     forward(request, connection, begin, end);
+    return true;
   }
   catch (std::exception &e)
   {
     ROS_WARN_STREAM("Error Handling Request: " << e.what());
+    return false;
   }
+  return false;
 }
 
 WebVideoServer::WebVideoServer(ros::NodeHandle &nh, ros::NodeHandle &private_nh) :
@@ -94,8 +97,8 @@ void WebVideoServer::cleanup_inactive_streams()
   if (lock)
   {
     typedef std::vector<boost::shared_ptr<ImageStreamer> >::iterator itr_type;
-    itr_type new_end = std::remove_if(image_subscribers_.begin(), image_subscribers_.end(),
-                                      boost::bind(&ImageStreamer::isInactive, _1));
+    itr_type new_end = std::partition(image_subscribers_.begin(), image_subscribers_.end(),
+                                      !boost::bind(&ImageStreamer::isInactive, _1));
     if (__verbose)
     {
       for (itr_type itr = new_end; itr < image_subscribers_.end(); ++itr)
